0f26e98933d10f660273ec92c14d027871f29ca5
youngmin <min050410@gmail.com>
2022-07-13 17:56:40 +0900

    add: gltfjsx로 3d 객체 생성

--- src/container/ThreeExemple/threeExemple.js
+++ src/container/ThreeExemple/threeExemple.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Suspense } from 'react';
 import { Canvas } from "@react-three/fiber";
 import Box from '../../components/Box';
+import { OrbitControls } from "@react-three/drei";
 
 // style
 import '../../styles/Canvas/canvas.css';
 import AnimatedSphere from '../../components/AnimatedSphere';
+import SmartCity from '../SmartCity/Smartcity';
 
 const ThreeExemple = () => { 
     return(
@@ -14,8 +16,14 @@ const ThreeExemple = () => {
             {/* <Suspense fallback={null}>
                 <Box/>
             </Suspense> */}
-            <Suspense fallback={null}>
+            {/* <Suspense fallback={null}>
                 <AnimatedSphere/>
+            </Suspense> */}
+            <Suspense fallback={null}>
+                <SmartCity />
+                <ambientLight intensity={0.5} />
+                <directionalLight position={[-2, 5, 2]} intensity={1} />
+                <OrbitControls enableZoom={false} />
             </Suspense>
         </Canvas>
     )
